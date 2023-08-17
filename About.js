069f665d3f377855fdfce8de72277af3a9acd6c3
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-08-17 14:15:46 +0500

    Add files via upload

---
+++ About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <section id="about" className="about">
+      <div className="about-content">
+        <h2>About Me</h2>
+        <p>My name is Sameer Ali Butt and i am a entry-level front-end developer from Gujar Khan, I got some  skills HTML5 - CSS3 - Javascript - React - Bootstrap - C++ - Database Managenet System - Object Oriented Programming, I have 4-5 months of learning experience and have done some basic projects.</p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
