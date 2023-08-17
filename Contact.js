db125b3443836a9f70307614a2de4aaa98ce3cff
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-08-17 14:19:26 +0500

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Contact.css';
+
+const Contact = () => {
+  return (
+    <section id="contact" className="contact">
+      <h2>Contact Me</h2>
+      <div className="contact-form">
+        <form>
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Your Email" />
+          <textarea placeholder="Your Message"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
